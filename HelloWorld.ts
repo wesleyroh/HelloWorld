6218d06464a245048dea60db70da8892620c6bca
Wesley Rodrigues <wesley.roh@hotmail.com>
2019-10-11 13:59:52 -0300

    Update Hello World PT/BR
    Hello World PT/BR

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,2 +1,10 @@
-const helloWorldString: string = 'Hello World';
-console.log(helloWorldString);
+// define the class with constructor
+class Author {
+    constructor (public Name: string) { }
+}
+// create the instance of the class
+let author = new Author("Wesley Mattos");
+// print the information in the console
+console.log("\nOlá Mundo,");
+console.log("Bem vindo ao mundo de");
+console.log(`\t~ ${author.Name}`);
